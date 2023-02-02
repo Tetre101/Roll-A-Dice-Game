5358e5f5efac6eaa38f69d6a987b0ad257ec8618
Tetre101 <basilpeter989@gmail.com>
2023-02-02 16:38:24 +0100

    updated highest score

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2.check if player's score is = 100. then finish the game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       dice.classList.add(`hidden`);
 
